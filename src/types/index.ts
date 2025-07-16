dc1f72b4fb6a7164196067bdb9fd2836c9cf2dd2
Chaitanya Sharma <19ucc010@lnmiit.ac.in>
2025-07-16 23:57:37 +0530

    update: types for events

---
+++ src/types/index.ts
@@ -0,0 +1,56 @@
+//list of possible event types
+export type InsightEventType =
+  | "click"
+  | "hover"
+  | "focus"
+  | "input"
+  | "scroll"
+  | "keydown"
+  | "custom";
+
+//format for recording 'scroll' event's data
+export interface ScrollData {
+  scrollTop: number;
+  scrollLeft: number;
+  scrollHeight: number;
+  scrollWidth: number;
+  clientHeight: number;
+  clientWidth: number;
+}
+
+export interface ElementDetails {
+  tag: string;
+  id?: string;
+  classList?: string[];
+  textContentSnippet?: string;
+}
+
+export interface Position {
+  x: number;
+  y: number;
+}
+
+export interface Context {
+  route?: string;
+  userId?: string;
+  sessionId?: string;
+  customContext?: Record<string, any>;
+}
+
+export interface InsightEvent {
+  id: string;
+  type: InsightEventType;
+  timeStamp: number;
+
+  //Optional depending on event type
+  element?: ElementDetails;
+  position?: Position;
+  scrollData?: ScrollData;
+
+  //Extra user-defined metadata
+  metadata?: Record<string, any>;
+  componentName?: string;
+
+  //App-level context (can be passed via InsightProvider)
+  context?: Context;
+}
